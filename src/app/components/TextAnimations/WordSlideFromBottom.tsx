4de99244be63e6ce7e4aeae4406d65b9e23bac63
Saidfatah <said_designer@outlook.com>
2024-08-21 16:18:11 +0100

    refactored TextRevealSlideFromBottom

--- src/app/components/TextAnimations/WordSlideFromBottom.tsx
+++ src/app/components/TextAnimations/WordSlideFromBottom.tsx
@@ -1,7 +1,9 @@
-function TextRevealSlideFromBottom({ text }: { text: string }) {
-    return (<p className="flex_wrapper rotateLineOut line_wrapper_Overflow">
-        {text.split(' ').map(word=><span className="slide_word" >{word}</span>)}
-    </p>);
+import clsx from 'clsx'
+
+function TextRevealSlideFromBottom({ text, className, index }: { text: string, className?: string, index?: number }) {
+    return (<span className={clsx("inline-block leading-[1em] will-change-transform overflow-hidden", className)}>
+        <span className={`slide_word_${(index ?? 0) + 1}`} >{text}</span>
+    </span>);
 }
 
 export default TextRevealSlideFromBottom;
